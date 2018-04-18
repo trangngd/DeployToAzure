51104889ec543cadc0e09870703ab8aac34e3ff7
trangngd <31686368+trangngd@users.noreply.github.com>
2018-04-18 22:44:15 +0300

    Update ctrlMenu.js

--- app_server/controllers/ctrlMenu.js
+++ app_server/controllers/ctrlMenu.js
@@ -1,10 +1,10 @@
 const request = require('request');
 const apiURL = require('./apiURLs');
 
-const menulist = function(req, res){
+ const menulist = function(req, res){
     const path = '/api/menu';
     const requestOptions = {
-        url : 'https://nguyentrang.azurewebsites.net/api/menu',
+        url : apiURL.server + path,
         method : 'GET',
         json : {},
         qs : {}
@@ -15,16 +15,21 @@ const menulist = function(req, res){
         function (err, response, body){
             if (err){
                 res.render('error', {message: err.message});
+                console.log("error");
             } else if (response.statusCode !== 200){
                 res.render('error', {message: 'Error accessing API: ' +
                     response.statusMessage +
                     " ("+ response.statusCode + ")" });
+                console.log("Erro accessing api");
             } else if (!(body instanceof Array)) {
                 res.render('error', {message: 'Unexpected response data'});
+                console.log("Unexpected data");
             } else if (!body.length){
                 res.render('error', {message: 'No documents in collection'});
+                console.log("body length");
             } else {
                 res.render('menu', {menus: body});
+                console.log("render " + body);
             }
         }
     );
