e6232a0f86927d9e54a5eb80fcb857355e19d3c3
trangngd <31686368+trangngd@users.noreply.github.com>
2018-04-18 23:50:20 +0300

    Update ctrlContact.js

--- app_server/controllers/ctrlContact.js
+++ app_server/controllers/ctrlContact.js
@@ -1,12 +1,33 @@
+const request = require('request');
+const apiURL = require('./apiURLs');
 
 const contactlist = function(req, res){
-    res.render('contact',{
-        contacts:
-            [
-                {note:'Location', detail:'Pohjois Haaga, Helsinki'},
-                {note:'Phone number', detail:'+358412345678'},
-                {note:'Email', detail:'helsinki@gmail.com'}
-            ]});
+    const path = '/api/contact';
+    const requestOptions = {
+        url : apiURL.server + path,
+        method : 'GET',
+        json : {},
+        qs : {}
+    };
+
+    request(
+        requestOptions,
+        function (err, response, body){
+            if (err){
+                res.render('error', {message: err.message});
+            } else if (response.statusCode !== 200){
+                res.render('error', {message: 'Error accessing API: ' +
+                    response.statusMessage +
+                    " ("+ response.statusCode + ")" });
+            } else if (!(body instanceof Array)) {
+                res.render('error', {message: 'Unexpected response data'});
+            } else if (!body.length){
+                res.render('error', {message: 'No documents in collection'});
+            } else {
+                res.render('contact', {contacts: body});
+            }
+        }
+    )
 };
 module.exports = {
     contactlist
