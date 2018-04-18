abf576380fbe862fd3f52ab4efac6e5cbe3a55d5
trangngd <31686368+trangngd@users.noreply.github.com>
2018-04-18 23:49:57 +0300

    Update ctrlMenu.js

--- app_server/controllers/ctrlMenu.js
+++ app_server/controllers/ctrlMenu.js
@@ -1,7 +1,7 @@
 const request = require('request');
 const apiURL = require('./apiURLs');
 
- const menulist = function(req, res){
+const menulist = function(req, res){
     const path = '/api/menu';
     const requestOptions = {
         url : apiURL.server + path,
@@ -15,24 +15,19 @@ const apiURL = require('./apiURLs');
         function (err, response, body){
             if (err){
                 res.render('error', {message: err.message});
-                console.log("error");
             } else if (response.statusCode !== 200){
                 res.render('error', {message: 'Error accessing API: ' +
                     response.statusMessage +
                     " ("+ response.statusCode + ")" });
-                console.log("Erro accessing api");
             } else if (!(body instanceof Array)) {
                 res.render('error', {message: 'Unexpected response data'});
-                console.log("Unexpected data");
             } else if (!body.length){
                 res.render('error', {message: 'No documents in collection'});
-                console.log("body length");
             } else {
                 res.render('menu', {menus: body});
-                console.log("render " + body);
             }
         }
-    );
+    )
 };
 module.exports = {
     menulist
