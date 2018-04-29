a40c5d19162c5c4f898592ea33ca750fb196b163
trangngd <31686368+trangngd@users.noreply.github.com>
2018-04-29 14:07:59 +0300

    Update index.js

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -3,12 +3,17 @@ const router = express.Router();
 
 // Declaring the controllers
 const ctrlMain = require('../controllers/ctrlMain'); // Controller for the Home page
-const ctrlMenu = require('../controllers/ctrlMenu'); // ... for the list of World Cup winners (countries)
-const ctrlContact = require('../controllers/ctrlContact'); // ... for the the list of Golden ball winners (players)
+const ctrlMenu = require('../controllers/ctrlMenu'); // ... for the list of menu
+const ctrlContact = require('../controllers/ctrlContact'); // ... for the the list of contact
 
 // Defining the routes and associating the routes to their corresponding controllers
 router.get('/', ctrlMain.index);  // Home page
-router.get('/menu', ctrlMenu.menulist);  // List of World Cup winners (countries)
-router.get('/contact', ctrlContact.contactlist);  // List of Golden ball winners (players)
+router.get('/menu', ctrlMenu.menulist);  // List of food
+router.get('/contact', ctrlContact.contactlist);  // List of contact
+
+router
+    .route('/menu/add')
+    .get(ctrlMenu.showForm)   // Display the form
+    .post(ctrlMenu.addData);
 
 module.exports = router;
